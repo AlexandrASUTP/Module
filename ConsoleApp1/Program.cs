2c7a67a9f7af2084c5564ee07276d1aedd09e187
Name <email@example.ru>
2022-04-17 00:10:28 +0300

    Create programm Anketa

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+		Console.Write("What is your name? ");
+		var name = Console.ReadLine();
+		Console.WriteLine("Your name is: {0}", name);
+		//-----------------------------
+		Console.Write("What is your age? ");
+		var age = checked((byte)int.Parse(Console.ReadLine()));
+		Console.WriteLine("Your age is: {0}", age);
+		//-----------------------------
+		Console.WriteLine("Your name is {0} and age is {1}", name, age);
+		//-----------------------------
+		Console.Write("What is your birtheday? ");
+		var day = Console.ReadLine();
+		Console.WriteLine("Your birtheday is {0}", day);
+	}
+}
